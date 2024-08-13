a9b9a7693966bcd0a3dcb71847874d0b45999a43
Rodrigo Manuel Cobos Rodríguez <hoop3.1416@gmail.com>
2024-08-13 15:35:17 -0600

    added statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 void main() {
+  print('se actualizó el programa');
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  var texto = 'Hello, World!';
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    print('se actualizó el widget App()');
+    
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(texto),
+              IconButton(
+                onPressed: () {
+                  texto = 'Hello, Jadid!';
+                  setState(() {});
+                },
+                icon: const Icon(Icons.person),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
